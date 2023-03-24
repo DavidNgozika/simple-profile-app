4d4ec4f7c1803c82feabc6ee92aeaead128a83be
David Ngozika <davidcngozika@gmail.com>
2023-03-24 14:10:18 +0100

    Style the UsersList component

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './UsersList.module.css';
 
 export default function UsersList({ users }) {
     return (
-        <ul>
+        <ul className={styles.container}>
             {users.map((user, index) => (
                 <li key={index}>
                     {user.name} {user.age}
